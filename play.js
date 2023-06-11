560bace2523a9369d83227e91ebc7cb344f3cae3
ggball_pc <1667834841@qq.com>
2023-06-11 11:45:37 +0800

    时间问题修复

--- play.js
+++ play.js
@@ -4,8 +4,9 @@ const { chromium } = require('@playwright/test');
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
-  // 获取今天日期转为字符串
-  const today = new Date().toISOString().slice(0, 10);
+  // 获取今天日期 往后的8个小时
+  const today = dateFormat(new Date(), 'yyyy-MM-dd')
+
 
   let mdContext = "## 每日prompt"+"\n"+"### "+today+"\n" + "<center class=\"half\">"
 
@@ -57,3 +58,25 @@ const { chromium } = require('@playwright/test');
 
   await browser.close();
 })();
+
+
+function dateFormat (date, format) {
+  date = new Date(date);
+  var o = {
+      'M+' : date.getMonth() + 1, //month
+      'd+' : date.getDate(), //day
+      'H+' : date.getHours()+8, //hour+8小时
+      'm+' : date.getMinutes(), //minute
+      's+' : date.getSeconds(), //second
+      'q+' : Math.floor((date.getMonth() + 3) / 3), //quarter
+      'S' : date.getMilliseconds() //millisecond
+  };
+  if (/(y+)/.test(format))
+      format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+
+  for (var k in o)
+      if (new RegExp('(' + k + ')').test(format))
+          format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+
+  return format;
+}
